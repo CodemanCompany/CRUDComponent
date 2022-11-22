ec4290e3f212712cfd293c81e283715f297b9d47
Gustavo Martinez <gustavo@codeman.company>
2022-11-22 08:23:51 -0600

    Feat: Without Datatables => Linux

--- src/CRUDComponent.js
+++ src/CRUDComponent.js
@@ -2,8 +2,8 @@ import './CRUDComponent.css';
 
 function CRUDComponent() {
 	return (
-		<div className="CRUDComponent">
-			<h1>CRUDComponent</h1>
+		<div className="CRUDComponent container">
+			<h1 className="display-5 my-3">CRUDComponent</h1>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import CRUDComponent from './CRUDComponent';
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
