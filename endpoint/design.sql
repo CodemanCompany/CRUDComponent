4ea2375ad5df36f3a27a1fb0b4893515a51b1228
Gustavo Martinez <gustavo@codeman.company>
2022-11-22 20:40:59 -0600

    Progress Linux

--- endpoint/design.sql
+++ endpoint/design.sql
@@ -33,5 +33,24 @@ CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO users
 VALUES
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "1@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "2@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "3@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "4@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "5@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "6@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "7@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "8@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "9@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "10@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "11@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "12@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "13@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "14@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "15@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "16@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "17@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "18@codeman.company", "M", NOW(), NOW(), NULL ),
+( DEFAULT, "Gustavo", "Martinez", "Miranda", "19@codeman.company", "M", NOW(), NOW(), NULL ),
 ( DEFAULT, "Gustavo", "Martinez", "Miranda", "gustavo@codeman.company", "M", NOW(), NOW(), NULL ),
 ( DEFAULT, "Mateo", "Martinez", "Bernal", "mateo.reload962@gmail.com", "M", NOW(), NOW(), NULL );
