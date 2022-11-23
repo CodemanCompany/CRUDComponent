4510f91151f95c7aca0d61f9f56d2217d3006e8f
Gustavo Martinez <gustavo@codeman.company>
2022-11-22 18:24:47 -0600

    Progress Linux

---
+++ base/src/CRUDComponent.js
@@ -0,0 +1,11 @@
+import './CRUDComponent.css';
+
+function CRUDComponent() {
+	return (
+		<div className="CRUDComponent container">
+			<h1 className="display-5 my-3">CRUDComponent</h1>
+		</div>
+	);
+}
+
+export default CRUDComponent;
